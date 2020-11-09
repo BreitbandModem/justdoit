a51145cf8bfeeae606ae299e3de6589d53d91a67
matze <matthias.bidlingmeyer@gmail.com>
2020-11-09 11:44:41 +0100

    Add delete enpoint to api

--- backend/api_controller.py
+++ backend/api_controller.py
@@ -2,13 +2,31 @@
 
 from habit_model import HabitModel
 
-import logging
 import json
 from jsonschema import validate, ValidationError, SchemaError
 from flask import Flask, Response, request, jsonify
 
 app = Flask(__name__)
 meditation_habit = None
+
+date_list_schema = {
+    "type": "object",
+    "definitions": {
+        "DateEntry": {
+            "properties": {"date": {"type": "string"}},
+            "required": ["date"],
+            "additionalProperties": False
+        }
+    },
+    "properties": {
+        "dates": {
+            "type": "array",
+            "items": {"$ref": "#/definitions/DateEntry"}
+        }
+    },
+    "additionalProperties": False,
+    "required": ["dates"]
+}
 
 @app.route('/habit/meditation', methods=['GET'])
 def get_dates():
@@ -28,66 +46,73 @@ def get_dates():
             validate(history_json, schema)
         except SchemaError as e:
             app.logger.error('Schema definition invalid.')
+            return Response(jsonify({'history': []}), status=500, mimetype='application/json')
         except ValidationError as e:
             app.logger.warning(e)
+            return Response(jsonify({'history': []}), status=400, mimetype='application/json')
         else:
             app.logger.info('Retreiving history for last %s days.', history_json['history'])
 
-    return jsonify({'date': "blabla"})
+    return Response(jsonify({'history': []}), status=500, mimetype='application/json')
 
 
 @app.route('/habit/meditation', methods=['POST'])
 def add_dates():
     """Submit a list of dates when the habit was done."""
-    schema = {
-        "type": "object",
-        "definitions": {
-            "DateEntry": {
-                "properties": {"date": {"type": "string"}},
-                "required": ["date"],
-                "additionalProperties": False
-            }
-        },
-        "properties": {
-            "dates": {
-                "type": "array",
-                "items": {"$ref": "#/definitions/DateEntry"}
-            }
-        },
-        "additionalProperties": False,
-        "required": ["dates"]
-    }
-
     dates = json.loads(request.data)
     if dates is not None:
         try:
-            validate(dates, schema)
+            validate(dates, date_list_schema)
         except SchemaError as e:
             app.logger.error('Schema definition invalid.')
+            return Response(jsonify({'added': 0}), status=500, mimetype='application/json')
         except ValidationError as e:
             app.logger.warning(e)
+            return Response(jsonify({'added': 0}), status=400, mimetype='application/json')
         else:
             app.logger.info('Adding list of dates: %s.', dates['dates'][0])
-            meditation_habit.add_dates(dates['dates'])
+            try:
+                added = meditation_habit.add_dates(dates['dates'])
+            except Exception:
+                return Response(jsonify({'added': 0}), status=500, mimetype='application/json')
+            else:
+                if added > 0:
+                    status = 201
+                else:
+                    status = 200
+                return Response(jsonify({'added': added}), status=status, mimetype='application/json')
 
-    return jsonify({'date': "9. Nov 2020"})
+    return Response(jsonify({'added': 0}), status=500, mimetype='application/json')
+
 
 
 @app.route('/habit/meditation', methods=['DELETE'])
 def delete_dates():
     """Delete a list of dates when a habit was not done."""
-    date = json.loads(request.data)
-    if date is not None:
-        app.logger.info('Date to be deleted: %s', date)
+    dates = json.loads(request.data)
+    if dates is not None:
+        try:
+            validate(dates, date_list_schema)
+        except SchemaError as e:
+            app.logger.error('Schema definition invalid.')
+            return Response(jsonify({'deleted': 0}), status=500, mimetype='application/json')
+        except ValidationError as e:
+            app.logger.warning(e)
+            return Response(jsonify({'deleted': 0}), status=400, mimetype='application/json')
+        else:
+            app.logger.info('Deleting list of dates: %s.', dates['dates'][0])
+            try:
+                deleted = meditation_habit.delete_dates(dates['dates'])
+            except Exception:
+                return Response(jsonify({'deleted': 0}), status=500, mimetype='application/json')
+            else:
+                return Response(jsonify({'deleted': deleted}), status=200, mimetype='application/json')
 
-    return jsonify({'result': 0})
+    return Response(jsonify({'deleted': 0}), status=500, mimetype='application/json')
 
 
 if __name__ == '__main__':
-    # Init Logger
-    logging.basicConfig(level=logging.DEBUG)
-    logging.basicConfig(filename='/logs/app.log', level=logging.DEBUG)
-    app.logger.info('Hello Logger')
+    app.logger.info('Starting Webserver')
 
     meditation_habit = HabitModel(app.logger, '/data/meditation.csv')
 
--- backend/habit_model.py
+++ backend/habit_model.py
@@ -18,6 +18,7 @@ class HabitModel:
 
     def add_dates(self, dates):
         """Store list of dates to csv file."""
+        add_count = 0
         with open(self.dates_filename, 'a+') as dates_file:
             for date in dates:
                 # if date (without time) doesn't exist in file, insert full date at end of file
@@ -25,6 +26,33 @@ class HabitModel:
                 if date["date"].split('T')[0] not in dates_file.read():
                     dates_file.seek(0, 2)  # seek to file end
                     dates_file.write(date["date"] + "\n")
+                    add_count += 1
+        return add_count
 
     def delete_dates(self, dates):
-        pass
+        """Delete list of dates from csv file."""
+        delete_count = 0
+        with open(self.dates_filename, 'r+') as dates_file:
+            # cut file contents to memory
+            dates_file.seek(0)
+            dates_file_lines = dates_file.readlines()
+
+            dates_file.seek(0)
+            dates_file.truncate()
+
+            # write all lines except for to be deleted back to file
+            for line in dates_file_lines:
+                date_found = False
+
+                for date in dates:
+                    self.logger.info('searching for date %s', date["date"])
+                    if date["date"].split('T')[0] in line:
+                        self.logger.info('found date in line: \n%s', line)
+                        date_found = True
+                        delete_count += 1
+
+                if not date_found:
+                    self.logger.info('writing line: \n%s', line)
+                    dates_file.write(line)
+
+        return delete_count
